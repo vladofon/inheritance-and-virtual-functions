b0d656f686be4a2867e1c67bb590ceb9ea9d584a
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-05-03 20:56:06 +0300

    Complete task

--- inheritance-and-virtual-functions/inheritance-and-virtual-functions.cpp
+++ inheritance-and-virtual-functions/inheritance-and-virtual-functions.cpp
@@ -1,7 +1,22 @@
 ﻿#include <iostream>
+#include "Function.h"
+#include "hyperbole.h"
+#include "line.h"
+#include "parabola.h"
+
+using namespace std;
 
 int main()
 {
-   std::cout << "Hello World!\n";
+   function* functions[3] = {
+      new hyperbole(10),
+      new line(10, 20, 30),
+      new parabola(10, 20, 30, 40)
+   };
+
+   for (function* func : functions)
+   {
+      cout << func->calculate() << endl;
+   }
 }
 
